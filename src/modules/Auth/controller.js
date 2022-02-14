62d003f6939fa4fda77f55d78baa623b4bf9369b
vunguyengitacc <nguyenhoanganhvu.it@gmail.com>
2022-02-14 09:09:33 +0700

    fix api

--- src/modules/Auth/controller.js
+++ src/modules/Auth/controller.js
@@ -34,11 +34,11 @@ const register = async (req, res, next) => {
     const { fullname, username, email, password } = req.body;
     const usernameChecker = await User.find({ username }).countDocuments();
     if (usernameChecker) {
-      return Result.error(res, { message: 'Username has already used' });
+      return Result.error(res, { message: 'Username has been already used' });
     }
     const emailChecker = await User.find({ email }).countDocuments();
     if (emailChecker) {
-      return Result.error(res, { message: 'Email has already used' });
+      return Result.error(res, { message: 'Email has been already used' });
     }
     const hash = await bcrypt.hash(password, 10);
     const newUser = await User.create({
